41fbd48c14e6b1951168a67d7dae233bfa9399e3
Kirill Khomicevich <khomicevichkirillwork@icloud.com>
2023-06-15 19:41:07 +0300

    Initial Commit

---
+++ simple_Realm_To_Do_List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_Realm_To_Do_List
+//
+//  Created by Kirill Khomicevich on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
